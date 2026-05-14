65d197b0ded073aa7e3c4deb04cfe7c6c1832172
Robert <rbocsa@me.com>
2026-05-14 11:25:22 +0200

    add admin users page

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,31 @@
+export default function UsersPage() {
+  return (
+    <div
+      style={{
+        padding: 40,
+        color: 'black'
+      }}
+    >
+      <h1
+        style={{
+          fontSize: 40,
+          fontWeight: 800,
+          marginBottom: 30
+        }}
+      >
+        Benutzerverwaltung
+      </h1>
+
+      <div
+        style={{
+          background: 'white',
+          padding: 30,
+          borderRadius: 20,
+          border: '1px solid #ddd'
+        }}
+      >
+        Benutzerverwaltung wird vorbereitet...
+      </div>
+    </div>
+  )
+}
